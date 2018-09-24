795a9269d4c978e12e7fa80ac11e70c556f155bf
Tory Dannenberg <rfdberg@gmail.com>
2018-09-24 15:42:36 -0600

    Add environment variables

--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -3,7 +3,7 @@ import { apiKey } from './apiKey.js';
 export const fetchTournaments = async () => {
   const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
 
-  const url = `${corsAnywhereUrl}http://api.sportradar.us/golf-t2/schedule/pga/2018/tournaments/schedule.json?api_key=${apiKey}`
+  const url = `${corsAnywhereUrl}http://api.sportradar.us/golf-t2/schedule/pga/2018/tournaments/schedule.json?api_key=${process.env.api_key || apiKey}`
 
   const response = await fetch(url, {
     method: 'GET'
@@ -16,7 +16,7 @@ export const fetchTournaments = async () => {
 export const fetchTournamentSummary = async (tournId) => {
   const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
 
-  const url = `${corsAnywhereUrl}http://api.sportradar.us/golf-t2/summary/pga/2018/tournaments/${tournId}/summary.json?api_key=${apiKey}`
+  const url = `${corsAnywhereUrl}http://api.sportradar.us/golf-t2/summary/pga/2018/tournaments/${tournId}/summary.json?api_key=${process.env.api_key || apiKey}`
 
   const response = await fetch(url, {
     method: 'GET'
