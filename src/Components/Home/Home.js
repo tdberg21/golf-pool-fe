0308e60fa10aeedc5cfa0fe5465905e2375e2417
Tory Dannenberg <rfdberg@gmail.com>
2018-09-24 11:34:39 -0600

    :lipstick: Update css for scrolling background and parallax text sections

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,17 +3,25 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div>
+    <div className='home-container'>
       <div className='parallax first-parallax'></div>
-      <h1>Hello, friends!</h1>
-      <p>Click on the tournament link above to start a draft</p>
+      <div className='parallax-text-sections welcome-section'>
+        <h2 className='welcome-header parallax-headers'>Hello, Friends.</h2>
+        <p>Click on the tournament link above to start a draft</p>
+      </div>
       <div className='parallax second-parallax'></div>
-      <h1>The Rules</h1>
-      <p>The draft order is selected at random.</p>
-      <p>Each team will alternate picks and draft players until each team has 5 players.</p>
-      <p>Only 4 players on each team will count towards the final score.</p>
-      <p>The team with the lowest cumulative score for their team is the winner.</p>
+      <div className='parallax-text-sections rules-section'>
+        <h2 className='parallax-headers'>The Rules</h2>
+        <p>The draft order is selected at random.</p>
+        <p>Each team will alternate picks and draft players until each team has 5 players.</p>
+        <p>Only 4 players on each team will count towards the final score.</p>
+        <p>The team with the lowest cumulative score is the winner.</p>
+      </div>
       <div className='parallax third-parallax'></div>
+      <div className='parallax-text-sections contestants-section'>
+        <h2 className='parallax-headers'>The Contestants</h2>
+        <p>Tory, Christy, Maddy, RJ, Kevin</p>
+      </div>
     </div>
   )
 }
