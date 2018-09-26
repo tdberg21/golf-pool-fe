5751e07607f5fb0f5860062c4e436a0cedbc8639
Tory Dannenberg <rfdberg@gmail.com>
2018-09-26 00:29:30 -0600

    Api calls for heroku

--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -1,30 +1,30 @@
 // import { apiKey } from './apiKey.js';
 
-export const fetchTournaments = async () => {
-  const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
+// export const fetchTournaments = async () => {
+//   const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
 
-  const url = `${corsAnywhereUrl}http://api.sportradar.us/golf-t2/schedule/pga/2018/tournaments/schedule.json?api_key=${apiKey}`
+//   const url = `${corsAnywhereUrl}http://api.sportradar.us/golf-t2/schedule/pga/2018/tournaments/schedule.json?api_key=${apiKey}`
 
-  const response = await fetch(url, {
-    method: 'GET'
-  });
-  const results = await response.json();
+//   const response = await fetch(url, {
+//     method: 'GET'
+//   });
+//   const results = await response.json();
 
-  return results;
-}
+//   return results;
+// }
 
-export const fetchTournamentSummary = async (tournId) => {
-  const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
+// export const fetchTournamentSummary = async (tournId) => {
+//   const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
 
-  const url = `${corsAnywhereUrl}http://api.sportradar.us/golf-t2/summary/pga/2018/tournaments/${tournId}/summary.json?api_key=${apiKey}`
+//   const url = `${corsAnywhereUrl}http://api.sportradar.us/golf-t2/summary/pga/2018/tournaments/${tournId}/summary.json?api_key=${apiKey}`
 
-  const response = await fetch(url, {
-    method: 'GET'
-  });
-  const results = await response.json();
+//   const response = await fetch(url, {
+//     method: 'GET'
+//   });
+//   const results = await response.json();
 
-  return results;
-}
+//   return results;
+// }
 
 // export const fetchLeaderboard = () => {
 //   const url = `http://api.sportradar.us/golf-t2/leaderboard/pga/2018/tournaments/${tournament_id}/leaderboard.json?api_key=${apiKey}`
