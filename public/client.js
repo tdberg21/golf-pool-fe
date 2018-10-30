fbf8bd6c5e1b5362f581e2fa8e9db3376c031730
Tory Dannenberg <rfdberg@gmail.com>
2018-10-30 10:30:25 -0600

    Add server and client files to try to get sockets to work WIP

---
+++ public/client.js
@@ -0,0 +1,7 @@
+// const socket = io();
+
+// const connectionCount = document.getElementById('connection-count');
+
+// socket.on('usersConnected', (count) => {
+//   connectionCount.innerText = 'Connected Users: ' + count;
+// });
