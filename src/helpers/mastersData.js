758823cf68afd02d9e075a42b812773dc6417b6e
Tory Dannenberg <rfdberg@gmail.com>
2018-12-19 14:10:35 -0700

    Add data for 2019 Masters

---
+++ src/helpers/mastersData.js
@@ -0,0 +1,134 @@
+export const mastersData = {
+  "id": "fdf12aec-74d2-4f2e-9e0b-00481a32fd34",
+  "name": "Masters Tournament",
+  "event_type": "stroke",
+  "purse": 11000000.0,
+  "winning_share": 1980000.0,
+  "currency": "USD",
+  "points": 600,
+  "start_date": "2019-04-11",
+  "end_date": "2019-04-14",
+  "course_timezone": "US/Eastern",
+  "seasons": [{
+    "id": "42cb037c-a813-49b3-a64e-1355e7bf4038",
+    "year": 2019,
+    "tour": {
+      "id": "b52068af-28e4-4e91-bdbb-037591b0ff84",
+      "alias": "pga",
+      "name": "PGA Tour"
+    }
+  }, {
+    "id": "6a06d55e-0f07-4b5b-8abf-c228fa554c4c",
+    "year": 2019,
+    "tour": {
+      "id": "e97bb87a-6347-40d0-8c8c-3d0f54a41043",
+      "alias": "EURO",
+      "name": "European Tour"
+    }
+  }],
+  "venue": {
+    "id": "d555598a-aed0-44fe-b659-4250264c7820",
+    "name": "Augusta National Golf Club",
+    "city": "Augusta",
+    "state": "GA",
+    "zipcode": "30904",
+    "country": "USA",
+    "courses": [{
+      "id": "7e9462a5-66ea-4205-b37a-81884e3653cf",
+      "name": "Augusta National",
+      "yardage": 7435,
+      "par": 72,
+      "holes": [{
+        "number": 1,
+        "par": 4,
+        "yardage": 445,
+        "description": "Tea Olive"
+      }, {
+        "number": 2,
+        "par": 5,
+        "yardage": 575,
+        "description": "Pink Dogwood"
+      }, {
+        "number": 3,
+        "par": 4,
+        "yardage": 350,
+        "description": "Flowering Pitch"
+      }, {
+        "number": 4,
+        "par": 3,
+        "yardage": 240,
+        "description": "Flowering Crabapple"
+      }, {
+        "number": 5,
+        "par": 4,
+        "yardage": 455,
+        "description": "Magnolia"
+      }, {
+        "number": 6,
+        "par": 3,
+        "yardage": 180,
+        "description": "Juniper"
+      }, {
+        "number": 7,
+        "par": 4,
+        "yardage": 450,
+        "description": "Pampas"
+      }, {
+        "number": 8,
+        "par": 5,
+        "yardage": 570,
+        "description": "Yellow Jasmine"
+      }, {
+        "number": 9,
+        "par": 4,
+        "yardage": 460,
+        "description": "Carolina Cherry"
+      }, {
+        "number": 10,
+        "par": 4,
+        "yardage": 495,
+        "description": "Camellia"
+      }, {
+        "number": 11,
+        "par": 4,
+        "yardage": 505,
+        "description": "White Dogwood"
+      }, {
+        "number": 12,
+        "par": 3,
+        "yardage": 155,
+        "description": "Golden Bell"
+      }, {
+        "number": 13,
+        "par": 5,
+        "yardage": 510,
+        "description": "Azalea"
+      }, {
+        "number": 14,
+        "par": 4,
+        "yardage": 440,
+        "description": "Chinese Fir"
+      }, {
+        "number": 15,
+        "par": 5,
+        "yardage": 530,
+        "description": "Firethorn"
+      }, {
+        "number": 16,
+        "par": 3,
+        "yardage": 170,
+        "description": "Redbud"
+      }, {
+        "number": 17,
+        "par": 4,
+        "yardage": 440,
+        "description": "Nandina"
+      }, {
+        "number": 18,
+        "par": 4,
+        "yardage": 465,
+        "description": "Holly"
+      }]
+    }]
+  }
+}
